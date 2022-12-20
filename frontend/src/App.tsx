adb40890499f26a9b1e9211980791739e1cd3373
RunesSI <rubenssi825@gmail.com>
2022-12-20 19:14:49 -0300

    Add imagens para botão

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from './components/notificationButton'
+
 function App() {
   return (
-   <h1>Olá Mundo!</h1>
+    <>
+      <h1>Olá Mundo!</h1>
+      <NotificationButton />
+    </>
+
   )
 }
 
